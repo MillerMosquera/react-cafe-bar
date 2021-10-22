09de91e3096e10d932c362fcb33c4f5235741768
Sebastian Ramirez <51139115+Sramirei@users.noreply.github.com>
2021-10-22 13:28:16 -0500

    contactenos

--- src/app/layout/contacto.jsx
+++ src/app/layout/contacto.jsx
@@ -1,16 +1,14 @@
 import 'bootstrap'
+import React from 'react';
+
+import ContactoC from '../components/contacto/contactoC';
+
 
 function contacto() {
   return (
-    <section id="contact" className="contact">
-      <div className="container" data-aos="fade-up">
-
-        <div className="section-title">
-          <h2>Contacto</h2>
-          <p>Reserva con nosotros</p>
-        </div>
-      </div>
-    </section>
+   <>
+  <ContactoC/>
+   </>
   );
 }
 
