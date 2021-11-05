d023e04fa5b4d198a35d02114dd08b1b1e3f1232
Miller Palacios <millerpalacios18@hotmail.com>
2021-11-05 00:45:25 -0500

    cambio de color de header y logo de la pagina

--- src/app/components/header/headerC.jsx
+++ src/app/components/header/headerC.jsx
@@ -12,7 +12,6 @@ export default function header() {
           collapseOnSelect
           expand="lg"
           className="fixed-top d-flex align-items-center "
-          bg="dark"
           variant="dark">
 
           <Navbar.Brand>
