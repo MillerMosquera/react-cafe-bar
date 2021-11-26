be13dc5bcf6d56806930f914c9d7fb3fa5fb946a
josegcort <josegcort@gmail.com>
2021-11-26 14:57:28 -0500

    Reserva con estado

--- src/app/components/servicios/serviciosC.jsx
+++ src/app/components/servicios/serviciosC.jsx
@@ -7,8 +7,8 @@ import { Link } from "react-router-dom";
 
 function ServiciosC() {
 
-    const [data, setData] = useState([]);
-   //function
+  const [data, setData] = useState([]);
+  //function
   const getData = async () => {
     const res = await axios.get(
       `https://cafecito-backend.herokuapp.com/api/servicio`
@@ -16,22 +16,25 @@ function ServiciosC() {
     const data = res.data;
     const postData = data.map((pd) => (
       <section key={pd.id} className="menu">
-         <div id="cumple" className="row">
-                        <div className="col-lg-5 col-md-6 align-self-center">
-                            <h1>{pd.numero}</h1>
-                            <div className="deshes-text">
-                                <h3><span>{pd.titulo}</span><br /></h3>
-                                <p className="pt-3">{pd.descripcion} </p>
-                            </div>
-                        </div>
-                        <div className="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
-                            <img src={pd.foto} alt="" className="img-fluid" />
-                        </div>
+        <div id="cumple" className="row">
+          <div className="col-lg-5 col-md-6 align-self-center">
+            <h1>{pd.numero}</h1>
+            <div className="deshes-text">
+              <h3><span>{pd.titulo}</span><br /></h3>
+              <p className="pt-3">{pd.descripcion} </p>
+            </div>
+          </div>
+          <div className="col-lg-5 offset-lg-2 col-md-6 align-self-center mt-4 mt-md-0">
+            <img src={pd.foto} alt="" className="img-fluid" />
+          </div>
         </div>
+        <br />
+        <br />
+        <br />
       </section>
     ));
     setData(postData);
-    
+
   };
 
   useEffect(() => {
@@ -43,31 +46,31 @@ function ServiciosC() {
   return (
     <>
       <section>
-            <div className="deshes-area section-padding">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
+        <div className="deshes-area section-padding">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
 
-                            <div className="section-title">
-                                <h2>Nuestros servicios para tí</h2>
-                                <p>Estos son algunos servicios para mejorar tu experiencia en nuestro restaurante.</p>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div className="servicios">{data}</div>
-
+                <div className="section-title">
+                  <h2>Nuestros servicios para tí</h2>
+                  <p>Estos son algunos servicios para mejorar tu experiencia en nuestro restaurante.</p>
                 </div>
+
+              </div>
             </div>
 
-            <div className="container-boton">
+            <div className="servicios">{data}</div>
+
+          </div>
+        </div>
+
+        <div className="container-boton">
           <Link to="/reserva">
             <img className="boton" src={reserved} alt="" />
           </Link>
         </div>
-        
-            </section>
+
+      </section>
     </>
   );
 }
